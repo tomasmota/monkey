c5f8eb9da6d3d70f6ecc5c1774e173e73c796813
tomasmota <tomasrebelomota@gmail.com>
2023-12-23 22:32:24 +0100

    first set of tokens in lexer

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -24,12 +24,33 @@ func (l *Lexer) readChar() {
 		l.ch = l.input[l.position]
 	}
 	l.position = l.readPosition
-	l.readPosition++
+	l.readPosition += 1
 }
 
 func (l *Lexer) nextToken() token.Token {
     var tok token.Token
 
+    switch l.ch {
+    case '=':
+        tok = newToken(token.ASSIGN, l.ch)
+    case ';':
+        tok = newToken(token.SEMICOLON, l.ch)
+    case '(':
+        tok = newToken(token.LPAREN, l.ch)
+    case ')':
+        tok = newToken(token.RPAREN, l.ch)
+    case '{':
+        tok = newToken(token.LBRACE, l.ch)
+    case '}':
+        tok = newToken(token.RBRACE, l.ch)
+    case ',':
+        tok = newToken(token.COMMA, l.ch)
+    case '+':
+        tok = newToken(token.PLUS, l.ch)
+    case 0:
+        tok.Literal = ""
+        tok.Type = token.EOF
+    }
     
     return tok
 }
