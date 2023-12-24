5adbea7cff3c7a8b439f01dfa2c34f5b9d801ad4
tomasmota <tomasrebelomota@gmail.com>
2023-12-24 11:40:04 +0100

    parsing identifiers and skipping whitespace

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,6 +1,7 @@
 package lexer
 
 import (
+	"fmt"
 	"monkey/token"
 )
 
@@ -12,8 +13,8 @@ type Lexer struct {
 }
 
 func New(input string) *Lexer {
-    l := &Lexer{input: input}
-    l.readChar()
+	l := &Lexer{input: input}
+	l.readChar()
 	return l
 }
 
@@ -28,34 +29,65 @@ func (l *Lexer) readChar() {
 }
 
 func (l *Lexer) nextToken() token.Token {
-    var tok token.Token
-    switch l.ch {
-    case '=':
-        tok = newToken(token.ASSIGN, l.ch)
-    case ';':
-        tok = newToken(token.SEMICOLON, l.ch)
-    case '(':
-        tok = newToken(token.LPAREN, l.ch)
-    case ')':
-        tok = newToken(token.RPAREN, l.ch)
-    case '{':
-        tok = newToken(token.LBRACE, l.ch)
-    case '}':
-        tok = newToken(token.RBRACE, l.ch)
-    case ',':
-        tok = newToken(token.COMMA, l.ch)
-    case '+':
-        tok = newToken(token.PLUS, l.ch)
-    case 0:
-        tok.Literal = ""
-        tok.Type = token.EOF
-    }
+	var tok token.Token
 
-    l.readChar()
-    
-    return tok
+	l.skipWhitespace()
+
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			tok.Type = token.LookupIdentType(tok.Literal)
+			fmt.Println(tok.Literal)
+			fmt.Println(tok.Type)
+			return tok
+		} else {
+			tok = newToken(token.ILLEGAL, l.ch)
+		}
+	}
+
+	l.readChar()
+
+	return tok
+}
+
+func (l *Lexer) skipWhitespace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
+}
+
+func (l *Lexer) readIdentifier() string {
+	start := l.position
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+	return l.input[start:l.position]
 }
 
 func newToken(tokenType token.TokenType, ch byte) token.Token {
-    return token.Token{Type: tokenType, Literal: string(ch)}
+	return token.Token{Type: tokenType, Literal: string(ch)}
+}
+
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
 }
--- token/token.go
+++ token/token.go
@@ -8,27 +8,39 @@ type Token struct {
 }
 
 const (
-    ILLEGAL = "ILLEGAL"
-    EOF = "EOF"
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
 
-    // Identifiers + Literals
-    IDENT = "IDENT" // add, foobar, x, y ...
-    INT = "INT" // 3424098
+	// Identifiers + Literals
+	IDENT = "IDENT" // add, foobar, x, y ...
+	INT   = "INT"   // 3424098
 
-    // Operators
-    ASSIGN = "="
-    PLUS = "+"
+	// Operators
+	ASSIGN = "="
+	PLUS   = "+"
 
-    // Delimiters
-    COMMA = ","
-    SEMICOLON = ";"
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
 
-    LPAREN = "("
-    RPAREN = ")"
-    LBRACE = "{"
-    RBRACE = "}"
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
 
-    // Keywords
-    FUNCTION = "FUNCTION"
-    LET = "LET"
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
 )
+
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+func LookupIdentType(ident string) TokenType {
+	if tokenType, ok := keywords[ident]; ok {
+		return tokenType
+	}
+	return IDENT
+}
