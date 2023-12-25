f12cb09a0d3c58d6b8caae02e0824b56dd517560
tomasmota <tomasrebelomota@gmail.com>
2023-12-25 15:54:42 +0100

    added first parser test

---
+++ parser/parser.go
@@ -0,0 +1,37 @@
+package parser
+
+import (
+	"monkey/ast"
+	"monkey/lexer"
+	"monkey/token"
+)
+
+type Parser struct {
+	l *lexer.Lexer
+
+	curToken  token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+    p := &Parser{l: l}
+
+    p.NextToken()
+    p.NextToken()
+
+    return p
+}
+
+func (p *Parser) NextToken() {
+    p.curToken = p.peekToken
+    p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+    program := &ast.Program{}
+    program.Statements = []ast.Statement{}
+
+    for p.curToken.Type != token.EOF {
+
+    }
+}
