945c5d30d6572f85d3b16da96318a160e171f4f2
tomasmota <tomasrebelomota@gmail.com>
2023-12-27 11:23:32 +0100

    basic statement parsing

--- parser/parser.go
+++ parser/parser.go
@@ -14,24 +14,76 @@ type Parser struct {
 }
 
 func New(l *lexer.Lexer) *Parser {
-    p := &Parser{l: l}
+	p := &Parser{l: l}
 
-    p.NextToken()
-    p.NextToken()
+	p.NextToken()
+	p.NextToken()
 
-    return p
+	return p
 }
 
 func (p *Parser) NextToken() {
-    p.curToken = p.peekToken
-    p.peekToken = p.l.NextToken()
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
 }
 
 func (p *Parser) ParseProgram() *ast.Program {
-    program := &ast.Program{}
-    program.Statements = []ast.Statement{}
+	program := &ast.Program{}
+	program.Statements = []ast.Statement{}
 
-    for p.curToken.Type != token.EOF {
+	for p.curToken.Type != token.EOF {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+		p.NextToken()
+	}
 
-    }
+	return program
+}
+
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.curToken.Type {
+	case token.LET:
+		return p.parseLetStatement()
+	default:
+		return nil
+	}
+}
+
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+	stmt := &ast.LetStatement{Token: p.curToken}
+
+	if !p.expectPeek(token.IDENT) {
+		return nil
+	}
+
+	stmt.Name = &ast.Identifier{
+		Token: p.curToken,
+		Value: p.curToken.Literal,
+	}
+
+	// TODO: skip until semicolon for now (only parse identifier)
+	if !p.curTokenIs(token.SEMICOLON) {
+		p.NextToken()
+	}
+
+	return stmt
+}
+
+func (p *Parser) curTokenIs(tokenType token.TokenType) bool {
+	return p.curToken.Type == tokenType
+}
+
+func (p *Parser) peekTokenIs(tokenType token.TokenType) bool {
+	return p.peekToken.Type == tokenType
+}
+
+func (p *Parser) expectPeek(tokenType token.TokenType) bool {
+	if p.peekTokenIs(tokenType) {
+		p.NextToken()
+		return true
+	} else {
+		return false
+	}
 }
