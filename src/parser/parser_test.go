eb86fec3f01f12db047524da1e843ae55d6e328d
tomasmota <tomasrebelomota@gmail.com>
2024-01-05 13:11:13 +0100

    tmp

--- src/parser/parser_test.go
+++ src/parser/parser_test.go
@@ -231,6 +231,40 @@ func testIntegerLiteral(t *testing.T, exp ast.Expression, value int64) bool {
     return true
 }
 
+func testIdentifier(t *testing.T, exp ast.Expression, value string) bool {
+    ident, ok := exp.(*ast.Identifier)
+    if !ok {
+        t.Errorf("exp is not *ast.Identifier. got=%T", exp)
+        return false
+    }
+    if ident.Value != value {
+        t.Errorf("expected ident.Value to be %s. got=%s", value, ident.Value)
+        return false
+    }
+    if ident.TokenLiteral() != value {
+        t.Errorf("expected ident.TokenLiteral to be %s. got=%s", value, ident.TokenLiteral())
+        return false
+    }
+    return true
+}
+
+func testLiteralExpression(t *testing.T, exp ast.Expression, expected interface{}) bool {
+    switch v := expected.(type) {
+    case int:
+        return testIntegerLiteral(t, exp, int64(v))
+    case int64:
+        return testIntegerLiteral(t, exp, v)
+    case string:
+        return testIdentifier(t, exp, v)
+    }
+    t.Errorf("type of exp not handled. got=%T", exp)
+    return false
+}
+
+func testInfixExpression()  {
+    // left off here
+}
+
 func TestParsingInfixExpressions(t *testing.T) {
     prefixTests:= []struct {
         input string
