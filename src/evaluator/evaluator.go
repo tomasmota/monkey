1b4282e8cc9682b2118794abd271c7f820a1b4fd
tomasmota <tomasrebelomota@gmail.com>
2024-03-07 21:46:26 +0100

    evaluating minus prefix expressions

--- src/evaluator/evaluator.go
+++ src/evaluator/evaluator.go
@@ -53,6 +53,8 @@ func evalPrefixExpression(operator string, right object.Object) object.Object {
 	switch operator {
 	case "!":
 		return evalBangOperatorExpression(right)
+	case "-":
+		return evalMinusPrefixOperatorExpression(right)
 	default:
 		return NULL
 	}
@@ -68,5 +70,16 @@ func evalBangOperatorExpression(right object.Object) object.Object {
 		return TRUE
 	default:
 		return FALSE
+	}
+}
+
+func evalMinusPrefixOperatorExpression(right object.Object) object.Object {
+	if right.Type() != object.INTEGER_OBJ {
+		return NULL
+	}
+
+	value := right.(*object.Integer).Value
+	return &object.Integer{
+		Value: -value,
 	}
 }
