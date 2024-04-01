16d95983d340c07e6c5594b24fca606070c65272
tomasmota <tomasrebelomota@gmail.com>
2024-04-01 21:07:44 +0200

    function calls fully implemented

--- src/evaluator/evaluator_test.go
+++ src/evaluator/evaluator_test.go
@@ -270,3 +270,21 @@ func TestFunctionObject(t *testing.T) {
 		t.Fatalf("body is not %s.. got=%s", expectedBody, fn.Body.String())
 	}
 }
+
+func TestFunctionApplication(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int64
+	}{
+		{"let identity = fn(x) { x; }; identity(5);", 5},
+		{"let identity = fn(x) { return x;}; identity(5);", 5},
+		{"let double = fn(x) { return x * 2;}; double(5);", 10},
+		{"let add = fn(x, y) { return x + y;}; add(5, 5);", 10},
+		{"let add = fn(x, y) { return x + y;}; add(5 + 5, add(5, 5));", 20},
+		{"fn(x) {x;}(5)", 5},
+	}
+
+	for _, tt := range tests {
+		testIntegerObject(t, testEval(tt.input), tt.expected)
+	}
+}
