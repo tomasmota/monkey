ff9e19f21a063311e6e44b0fa00aef4d4d2087e4
tomasmota <tomasrebelomota@gmail.com>
2024-04-05 20:57:14 +0200

    .

--- src/ast/ast_test.go
+++ src/ast/ast_test.go
@@ -6,32 +6,32 @@ import (
 )
 
 func TestString(t *testing.T) {
-    program := &Program{
-        Statements: []Statement{
-            &LetStatement{
-                Token: token.Token{
-                    Type: token.LET,
-                    Literal: "let",
-                },
-                Name: &Identifier{
-                    Token: token.Token{
-                        Type: token.IDENT, 
-                        Literal: "x",
-                    },
-                    Value: "x",
-                },
-                Value: &Identifier{
-                    Token: token.Token{
-                        Type: token.IDENT, 
-                        Literal: "y",
-                    },
-                    Value: "y",
-                },
-            },
-        },
-    }
+	program := &Program{
+		Statements: []Statement{
+			&LetStatement{
+				Token: token.Token{
+					Type:    token.LET,
+					Literal: "let",
+				},
+				Name: &Identifier{
+					Token: token.Token{
+						Type:    token.IDENT,
+						Literal: "x",
+					},
+					Value: "x",
+				},
+				Value: &Identifier{
+					Token: token.Token{
+						Type:    token.IDENT,
+						Literal: "y",
+					},
+					Value: "y",
+				},
+			},
+		},
+	}
 
-    if program.String() != "let x = y;" {
-        t.Errorf("program.String() wrong. got=%q", program.String())
-    }
+	if program.String() != "let x = y;" {
+		t.Errorf("program.String() wrong. got=%q", program.String())
+	}
 }
